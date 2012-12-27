e434d520ad4e5fbb01749db916508bbe7bf1de28
Quan Nguyen <ntquan1988@gmail.com>
2012-12-27 20:23:04 +0100

    added some description and modified login

--- verilinks-server/src/main/webapp/js/auth.js
+++ verilinks-server/src/main/webapp/js/auth.js
@@ -1,36 +1,63 @@
-var OAUTHURL = 'https://accounts.google.com/o/oauth2/auth?';
-var VALIDURL = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=';
-var SCOPE = 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
-var CLIENTID = '23285137063-h9ddnqshh1924a3808n78sgkdvfskjme.apps.googleusercontent.com';
-var REDIRECT = 'http://127.0.0.1:8020/verilinks-server/oauth.html';
-var LOGOUT = 'http://accounts.google.com/Logout';
-var TYPE = 'token';
-var _url = OAUTHURL + 'scope=' + SCOPE + '&client_id=' + CLIENTID + '&redirect_uri=' + REDIRECT + '&response_type=' + TYPE;
 var acToken;
 var tokenType;
 var expiresIn;
 var user;
 var loggedIn = false;
+var REDIRECT = 'http://127.0.0.1:8020/verilinks-server/oauth.html';
+var VALIDURL;
+// Google
+var login_google = {
+	'oauthUrl' : 'https://accounts.google.com/o/oauth2/auth?',
+	'validUrl' : 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=',
+	'clientId' : '23285137063-h9ddnqshh1924a3808n78sgkdvfskjme.apps.googleusercontent.com',
+	'logout' : 'http://accounts.google.com/Logout',
+	'scope' : 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+	'type' : 'token'
+};
+// facebook
+var login_facebook = {
+	oauthUrl : 'https://www.facebook.com/dialog/oauth?',
+	validUrl : 'https://graph.facebook.com/me?access_token=',
+	clientId : '468976209814720',
+	logout : 'https://www.facebook.com/logout.php?next=' + REDIRECT + '&access_token=',
+	scope : 'email',
+	type : 'token'
+};
+
+function getLogin() {
+	var login = $("#login").val();
+	var url;
+	if (login == 'facebook') {
+		url = login_facebook.oauthUrl + 'scope=' + login_facebook.scope + '&client_id=' + login_facebook.clientId + '&redirect_uri=' + REDIRECT + '&response_type=' + login_facebook.type
+		VALIDURL = login_facebook.validUrl;
+	} else {
+		url = login_google.oauthUrl + 'scope=' + login_google.scope + '&client_id=' + login_google.clientId + '&redirect_uri=' + REDIRECT + '&response_type=' + login_google.type
+		VALIDURL = login_google.validUrl;
+	}
+	return url;
+}
 
 function login() {
-	var win = window.open(_url, "windowname1", 'width=800, height=600');
+	var url = getLogin();
+	var win = window.open(url, "windowname1", 'width=800, height=600');
 
 	var pollTimer = window.setInterval(function() {
-		try{
-		console.log(win);
-		console.log(win.document);
-		console.log(win.document.URL);
-		if (win.document.URL.indexOf(REDIRECT) != -1) {
-			window.clearInterval(pollTimer);
-			var url = win.document.URL;
-			acToken = gup(url, 'access_token');
-			tokenType = gup(url, 'token_type');
-			expiresIn = gup(url, 'expires_in');
-			win.close();
+		try {
+			console.log(win);
+			console.log(win.document);
+			console.log(win.document.URL);
+			if (win.document.URL.indexOf(REDIRECT) != -1) {
+				window.clearInterval(pollTimer);
+				var url = win.document.URL;
+				acToken = gup(url, 'access_token');
+				tokenType = gup(url, 'token_type');
+				expiresIn = gup(url, 'expires_in');
+				win.close();
 
-			validateToken(acToken);
+				validateToken(acToken);
+			}
+		} catch(ex) {
 		}
-		} catch(ex){}
 	}, 500);
 }
 
@@ -39,28 +66,47 @@ function validateToken(token) {
 		url : VALIDURL + token,
 		data : null,
 		success : function(responseText) {
-			getUserInfo();
+			getUserInfo(responseText);
 			loggedIn = true;
-			$('#loginText').hide();
-			$('#logoutText').show();
+			showUser();
 			showAdmin();
 		},
 		dataType : "jsonp"
 	});
 }
 
-function getUserInfo() {
-	$.ajax({
-		url : 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + acToken,
-		data : null,
-		success : function(resp) {
-			user = resp;
-			console.log(user);
-			$('#uName').text('Welcome ' + user.name);
-			$('#imgHolder').attr('src', user.picture);
-		},
-		dataType : "jsonp"
-	});
+function getUserInfo(response) {
+	if (response == null) {
+		alert("Login error!");
+		return;
+	}
+	// facebook
+	if ($("#login").val() == 'facebook') {
+		// name
+		$('#userName').text('Name: ' + response.name);
+		// mail
+		$('#userMail').text('Mail: ' + response.email);
+		// profile picture
+		$.ajax({
+			url : 'http://graph.facebook.com/' + response.username + '/picture',
+			data : null,
+			success : function(pic) {
+				$('#userPicture').attr('src', pic.data.url);
+			},
+			dataType : "jsonp"
+		});
+	} else// google
+		$.ajax({
+			url : 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + acToken,
+			data : null,
+			success : function(resp) {
+				// name
+				$('#userName').text('Name: ' + resp.name);
+				// mail
+				$('#userMail').text('Mail: ' + resp.email);
+			},
+			dataType : "jsonp"
+		});
 }
 
 //credits: http://www.netlobo.com/url_query_string_javascript.html
@@ -76,13 +122,35 @@ function gup(url, name) {
 }
 
 function startLogoutPolling() {
-	$('#loginText').show();
-	$('#logoutText').hide();
 	loggedIn = false;
-	$('#uName').text('Welcome ');
-	$('#imgHolder').attr('src', 'none.jpg');
+	$('#userPanel').hide();
+	// enable
+	$('#loginPanel :input').removeAttr('disabled');
+	// change text
+	$(".button-big").text('Login');
+	$(".button-big").removeAttr('onCLick');
+	setTimeout('$(".button-big").click(function() {login();})', 2000);
+
 }
 
-function showAdmin(){
-	
+function showUser() {
+	$('#userPanel').show();
+	// disable
+	$('#loginPanel :input').attr('disabled', true);
+
+	// logout
+	$(".button-big").text('Logout');
+	$(".button-big").removeAttr('onCLick');
+	$(".button-big").click(function() {
+		if ($('#login').val() == 'google')
+			myIFrame.location = login_google.logout;
+		else
+			myIFrame.location = login_facebook.logout + acToken;
+		startLogoutPolling();
+		return false;
+	});
+}
+
+function showAdmin() {
+
 }
